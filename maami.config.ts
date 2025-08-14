877ce0dd3c1fc9717e0e602d39cafa57f55f6698
Akshay Narayanan Balajee Viswanath <akshayna001@e.ntu.edu.sg>
2025-08-14 17:23:00 +0800

    Update maami.config.ts

--- maami.config.ts
+++ maami.config.ts
@@ -33,7 +33,7 @@ const MaamiConfig: Configuration = {
 
   username: "Sollungo Maami",
   sign: "Har Har Mahadev",
-  avatarUrl: "https://s2.loli.net/2025/01/25/FPpTrQSezM8ivbl.webp",
+  avatarUrl: "https://github.com/AkshayNarayananB/SollungoMaami/edit/master/IMG_20220308_092327.jpg",
   socialLinks: [
     {
       icon: "mdi:instagram",
