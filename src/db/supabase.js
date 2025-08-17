db97c724622d51d5c09d0c9a5cb890049881bd8f
Akshay Narayanan Balajee Viswanath <akshayna001@e.ntu.edu.sg>
2025-08-17 12:37:44 +0800

    Update supabase.js

--- src/db/supabase.js
+++ src/db/supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'npm:@supabase/supabase-js@2.39.0';
+import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
