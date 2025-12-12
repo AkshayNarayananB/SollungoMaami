96f300c01364c6545f92cacd2eeb65d0069ea201
bvidya92 <vidyabalajee92@gmail.com>
2025-12-12 10:24:43 +0800

    Update firebase.js added authentication

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,20 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
 //import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlu-HLrVOjxvttYEWae5ks8YmBxUNuBnM",
-  authDomain: "sollungo-maami.firebaseapp.com",
-  projectId: "sollungo-maami",
-  storageBucket: "sollungo-maami.firebasestorage.app",
-  messagingSenderId: "776648875112",
-  appId: "1:776648875112:web:d132681846ec7a058fefdd",
-  measurementId: "G-YWX7544W5S"
+  apiKey: "AIzaSyB0D4w5zcP8XfNHaID5PKHAjmrK6W_kkfE",
+  authDomain: "sol-maami.firebaseapp.com",
+  projectId: "sol-maami",
+  storageBucket: "sol-maami.firebasestorage.app",
+  messagingSenderId: "95079224506",
+  appId: "1:95079224506:web:101c6ec34cf9a17daa1e83"
 };
 
 // Initialize Firebase
@@ -22,3 +24,7 @@ const app = initializeApp(firebaseConfig);
 //const analytics = getAnalytics(app);
 
 export const db = getFirestore(app);
+
+//auth
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
