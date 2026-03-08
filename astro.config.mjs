8f26d1033918ff103cdc6b5e6fb140c0f7df7f77
Akshay Narayanan Balajee Viswanath <akshayna001@e.ntu.edu.sg>
2026-03-08 20:37:45 +0800

    Change output mode from 'static' to 'hybrid'

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,7 +21,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: MaamiConfig.site,
-  output: 'static',
+  output: 'hybrid',
   adapter: vercel(),
   integrations: [
     tailwind(),
